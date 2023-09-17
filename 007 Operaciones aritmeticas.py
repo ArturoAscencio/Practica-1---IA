30ad7d13c0ce329b78cc51eb4b14e32aeebe5534
ArturoAscencio <arturo.ascencio.r@gmail.com>
2023-09-16 19:44:50 -0600

    Create d26e6988-f9e2-40a3-897b-678ecf82b497.vsidx

---
+++ 007 Operaciones aritmeticas.py
@@ -0,0 +1,7 @@
+suma =8+2
+resta =8-2
+multiplicacion = 8*2
+division = 8/2
+print("Suma:", suma, "Resta:", resta, "\nMultiplicacion: ", multiplicacion, "Division: ", division )  
+#La coma se utiliza para concatenar
+
